686827e9065751bbd91942e85c0f5e96077dbf69
vallzin <valmilson@gmail.com>
2024-05-23 13:58:56 -0300

    criação classe Item

--- collections-java-api-2024/collections-java-api-2024/src/main/java/list/OperacoesBasicas/ListaTarefa.java
+++ collections-java-api-2024/collections-java-api-2024/src/main/java/list/OperacoesBasicas/ListaTarefa.java
@@ -33,4 +33,19 @@ public class ListaTarefa {
         System.out.println(tarefaList);
     }
     
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+
+        System.out.println("O número total de elementos na lista é "+listaTarefa.obterNumeroTotalTarefas());
+        
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+        System.out.println("O número total de elementos na lista é "+listaTarefa.obterNumeroTotalTarefas());
+        
+        listaTarefa.removerTarefa("Tarefa 1");
+        System.out.println("O número total de elementos na lista é "+listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.obterDescricaoTaregas();
+    }
 }
